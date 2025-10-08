e6f072c5c75b5c16dba85a03d3af4c71f85920e4
Bryan Cuneo <bryan.cuneo@gmail.com>
2025-10-07 20:57:39 -0500

    Start of filestructure for log viewer webapp

---
+++ jsonlogger-parser/cmd/log_viewer/handlers.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+var tmpl *template.Template
+
+func init() {}
+
+func ShowBasePage(resWriter http.ResponseWriter, req *http.Request) {}
